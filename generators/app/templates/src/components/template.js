f789d36dfbdbf9a1354e96cfbcaf5efbe586410b
afeiship <1290657123@qq.com>
2017-04-21 15:32:29 +0800

    update

--- generators/app/templates/src/components/template.js
+++ generators/app/templates/src/components/template.js
@@ -1,6 +1,8 @@
 import './style.scss';
-import React,{PureComponent,PropTypes} from 'react';
+import React,{PureComponent} from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import noop from 'noop';
 
 export default class extends PureComponent{
   static propTypes = {
