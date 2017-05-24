a308cfac8b80f91f2bd40a34e9d4dd3de34fbd39
afeiship <1290657123@qq.com>
2017-05-24 20:28:16 +0800

    update tmplRE

--- generators/app/templates/build/auto-readme.js
+++ generators/app/templates/build/auto-readme.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const fileRegExp = require('file-regexp');
 const exampleRE = /\/\*===example start===\*\/\n([a-zA-Z.{\s()<="-<_>}]*)\/\*===example end===\*\//;
-const templateRE = /\/\*===properties start===\*\/([\w\s={}:.;,'"]*)\/\*===properties end===\*\//;
+const templateRE = /\/\*===properties start===\*\/([()\[\]\w\s={}:.;,'"]*)\/\*===properties end===\*\//;
 
 const targetPropsRE = /## properties:\n```javascript\n(.*)\n```/;
 const targetUsageRE = /## usage:\n```jsx\n(.*)\n```/;
