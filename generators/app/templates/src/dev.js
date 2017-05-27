f5ddfb492044e22a5f1499cb1b1219c63440ba73
afeiship <1290657123@qq.com>
2017-05-27 15:39:32 +0800

    otmiz

--- generators/app/templates/src/dev.js
+++ generators/app/templates/src/dev.js
@@ -2,6 +2,10 @@ import './dev.scss';
 import <%=ProjectName%> from './main';
 
 /*===example start===*/
+
+// install: npm install afeiship/<%=project_name%> --save
+// import : import <%=ProjectName%> from '<%=project_name%>'
+
 class App extends React.Component{
   render(){
     return (
