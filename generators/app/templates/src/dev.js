8196297b6404fdb3ebdd45a0d8f75d9c6363c64f
afeiship <1290657123@qq.com>
2017-06-09 16:40:59 +0800

    update dev code

--- generators/app/templates/src/dev.js
+++ generators/app/templates/src/dev.js
@@ -7,10 +7,21 @@ import <%=ProjectName%> from './main';
 // import : import <%=ProjectName%> from '<%=project_name%>'
 
 class App extends React.Component{
+  state = {
+
+  };
+
+  constructor(props){
+    super(props);
+    window.demo = this;
+    window.refs = this.refs;
+    window.rc = this.refs.rc;
+  }
+
   render(){
     return (
       <div className="hello-<%=project_name%>">
-        <<%=ProjectName%> />
+        <<%=ProjectName%> refs='rc' />
     </div>
     );
   }
