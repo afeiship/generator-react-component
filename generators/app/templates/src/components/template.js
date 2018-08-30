8a5d8e72c6019b653b6e5148197fbc89db48d787
afeiship <1290657123@qq.com>
2018-08-30 13:51:37 +0800

    issue: update tmpl

--- generators/app/templates/src/components/template.js
+++ generators/app/templates/src/components/template.js
@@ -1,17 +1,21 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import noop from 'noop';
 import objectAssign from 'object-assign';
 
-export default class extends Component{
+export default class extends Component {
   /*===properties start===*/
   static propTypes = {
-    className: PropTypes.string
+    className: PropTypes.string,
+    value: PropTypes.object,
+    onChange: PropTypes.func
   };
 
   static defaultProps = {
+    value: null,
+    onChange: noop
   };
   /*===properties end===*/
 
@@ -21,10 +25,10 @@ export default class extends Component{
     };
   }
 
-  render(){
+  render() {
     const { className, ...props } = this.props;
     return (
-      <div {...props} className={classNames('<%=project_name%>',className)} />
+      <div className={classNames('<%=project_name%>', className)} {...props} />
     );
   }
 }
