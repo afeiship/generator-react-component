d6547520cc985c43d0758be85be79a5cb78bd15f
afeiship <1290657123@qq.com>
2022-05-02 11:15:14 +0800

    feat: compositeWith base

--- generators/base/index.js
+++ generators/base/index.js
@@ -2,13 +2,17 @@
 const Generator = require('yeoman-generator');
 const globby = require('globby');
 const yoHelper = require('@jswork/yeoman-generator-helper');
-const MAIN = '@jswork/react-component';
+const genp = require('@jswork/generator-prompts');
+const prompts = genp(['scope', 'registry', 'project_name', 'description']);
 
 module.exports = class extends Generator {
+  async prompting() {
+    // Have Yeoman greet the user.
+    this.props = await this.prompt(prompts);
+  }
+
   writing() {
     const ctx = yoHelper.ctx;
-    this.composeWith(`${MAIN}:base`);
-
     this.fs.copyTpl(
       globby.sync(this.templatePath('**'), { dot: true }),
       this.destinationPath(),
