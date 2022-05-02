c362d809aa7c11d8dee0832c5ec6795165533c8c
afeiship <1290657123@qq.com>
2022-05-02 10:43:36 +0800

    fix: generator tmpl

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,16 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
 const yosay = require('yosay');
-const glob = require('glob');
+const globby = require('globby');
 const { resolve } = require('path');
-const remote = require('yeoman-remote');
-const replace = require('replace-in-file');
 const prompts = require('./prompts');
 const yoHelper = require('@jswork/yeoman-generator-helper');
-
-require('@jswork/next-capitalize');
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(
-        `Welcome to the stunning ${chalk.red(
-          'generator-react-component'
-        )} generator!`
-      )
+      yosay(`Welcome to the stunning 'generator-react-component' generator!`)
     );
 
     return this.prompt(prompts).then((props) => {
@@ -31,7 +22,7 @@ module.exports = class extends Generator {
   writing() {
     const { type } = this.props;
     this.fs.copyTpl(
-      glob.sync(resolve(this.templatePath(type), '{**,.*}')),
+      globby.sync(resolve(this.templatePath(type), '{**,.*}')),
       this.destinationPath(),
       this.props
     );
