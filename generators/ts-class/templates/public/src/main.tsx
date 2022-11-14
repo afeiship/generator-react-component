1d31b97f3e827f9178f0e4dff046643f1130a2fa
afeiship <1290657123@qq.com>
2022-11-14 10:27:58 +0800

    feat: rm unused tmpls

--- generators/ts-class/templates/public/src/main.tsx
+++ generators/ts-class/templates/public/src/main.tsx
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import '../../src/components/style.scss';
 import App from './app';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
